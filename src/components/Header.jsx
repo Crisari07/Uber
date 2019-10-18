c15e43ebfdf5ad5b28783b76572e19a04ab5338f
Ismael López <lc.ismael@hotmail.com>
2019-10-18 17:53:30 -0500

    created styles to menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import gravatar from '../utils/gravatar';
 import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 
-// import logo from '../assets/static/logo.ext';
+import logo from '../assets/static/cuber-logo.png';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = (props) => {
@@ -19,17 +20,28 @@ const Header = (props) => {
   return (
     <header className="header">
       <Link to="/">
-        <img className="header__img" src="" alt="Cuber" />
+        <img className="header__img" src={logo} alt="Cuber" />
       </Link>
       <div className="header__menu">
-        <div className="header__menu--profile">
-          {hasUser ? (
-            <img src={gravatar(user.email)} alt={user.email} />
-          ) : (
-            <img src={userIcon} alt="" />
-          )}
-          <p>Perfil</p>
+        <div className="header__menu--options">
+          <button className="button" type="button">
+            <Link to="/login">
+            Iniciar sesión
+            </Link>
+          </button>
+          <button className="button" type="button">
+            <Link to="/register">
+            Regístrate
+            </Link>
+          </button>
         </div>
+        {hasUser ? (
+        <div className="header__menu--profile">          
+          <>
+          <img src={gravatar(user.email)} alt={user.email} /> <p>Perfil</p>
+          </>         						          
+        </div>
+         ) : null }
         <ul>
           {hasUser ? (
             <li><a href="/">{user.name}</a></li>
